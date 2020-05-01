1bddb5649383bade09fc00010a542cb8676301aa
Alan Ashworth <awashworth927@gmail.com>
2020-05-01 19:04:08 -0400

    Add back react styles to project files. Add navbar animation to logo.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import Detail from "./Detail";
-
-// const List = (props) => {
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         <h1>You have {props.employees.length} employees.</h1>
-//       </div>
-//       <div>
-//             {props.employees.map((employee) => (
-//               <Detail {...employee} key={employee.id}/>
-//             ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default List;
-
 import React from "react";
 import Detail from "./Detail";
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,51 @@
 import React from "react";
+import logo from '../logo.svg';
+import '../App.css';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-dark bg-primary">
-      <a className="navbar-brand" href="/home">
-        Home
-      </a>
-      <a className="navbar-brand" href="/directory">
-        Directory
-      </a>
-      <form className="form-inline my-2 my-lg-0">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
-        </button>
-      </form>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <img className="navbar-brand App-logo" href="/" src={logo} alt="logo"></img>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/home">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/directory">
+              Directory
+            </a>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
     </nav>
   );
 };
