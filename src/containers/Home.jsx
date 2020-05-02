e917f4c64259c5a8a067173bb8c946fa6ae5a3e4
Alan Ashworth <awashworth927@gmail.com>
2020-05-01 20:15:32 -0400

    Create generic Home splash screen.

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import logo from "../logo.svg";
+import "../App.css";
 
 const Home = () => {
   return (
     <div className="container">
       <div className="row">
         <div className="col">
-          <h1>Welcome to Employee Tracker</h1>
+          <img src={logo} alt="logo"></img>
+        </div>
+      </div>
+
+      <div className="row App">
+        <div className="col">
+          <span>
+            <h1>Welcome to Employee Tracker</h1>
+          </span>
         </div>
       </div>
     </div>
