6bc38627f495f469453cf472e10c934b2799b7c2
Alan Ashworth <awashworth927@gmail.com>
2020-06-13 16:33:41 -0400

    Working on multiple column sort for Table component.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,131 +1,127 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 
 /**
- * * Componet set as a React Hooks component. Component demonstrates the
- * * use of hooks. Fetched data is retrieved as props from Directory
- * * container.
- * @param {*} data Props destructured. Arguements from Directory container
+ * Code resource from:
+ * https://www.smashingmagazine.com/2020/03/sortable-tables-react/
+ * @param {*} items 
+ * @param {*} config 
  */
+const useSortableData = (items, config = null) => {
+  const [sortConfig, setSortConfig] = useState(config);
+
+  const sortedItems = useMemo(() => {
+    let sortableItems = [...items];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [items, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return { items: sortedItems, requestSort, sortConfig };
+};
+
+// FIXME: Sorting not functional on columns where table data
+// has narrowing scope (i.e "location.counrty" or "name.first")
 const Table = ({ data }) => {
-  const [users, setUsers] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  useEffect(() => {
-    setUsers(data);
-  }, [data]);
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleClear = () => {
-    setUsers(data);
-    setSearchTerm("");
-  };
-
-  // TODO: update function to sort descending
-  const handleSort = (event, field) => {
-    event.preventDefault();
-
-    const userList = [...users];
-    const sortedByField = userList.sort((a, b) => {
-      switch (field) {
-        case "photo":
-          a = a.picture.thumbnail;
-          b = b.picture.thumbnail;
-          break;
-        case "name":
-          a = a.name.first;
-          b = b.name.first;
-          // a = `${a.name.first.toLowerCase()} ${a.name.last.toLowerCase()}`;
-          // b = `${b.name.first.toLowerCase()} ${b.name.last.toLowerCase()}`;
-          break;
-        case "email":
-          a = a.email;
-          b = b.email;
-          break;
-        case "phone":
-          a = a.phone;
-          b = b.phone;
-          break;
-        case "location":
-          a = a.location.country;
-          b = b.location.country;
-          break;
-        case "gender":
-          a = a.gender;
-          b = b.gender;
-          break;
-        default:
-          break;
-      }
-      if (a < b) return -1;
-      else if (a > b) return 1;
-      return 0;
-    });
-    setUsers(sortedByField);
-  };
-
-  // TODO: filter w/ last names
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const userList = [...users];
-    const filteredUsers = userList.filter((user) => {
-      const regex = new RegExp(searchTerm, "gi");
-      return user.name.first.match(regex);
-    });
-    setUsers(filteredUsers);
+  const { items, requestSort, sortConfig } = useSortableData(data);
+  const getClassNamesFor = (name) => {
+    if (!sortConfig) {
+      console.log("not sortconfig");
+      return;
+    }
+    console.log(`sort config`, sortConfig);
+    return sortConfig.key === name ? sortConfig.direction : undefined;
   };
 
   return (
     <>
-      <form
-        className="form-inline my-2 my-lg-0"
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          className="form-control mr-sm-2"
-          placeholder="Search name"
-          aria-label="Search"
-          name="searchTerm"
-          value={searchTerm}
-          onChange={handleChange}
-          autoComplete="off"
-        />
-        <button
-          className="btn btn-outline-primary my-2 mr-sm-2"
-          type="submit"
-        >
-          Search
-        </button>
-        <button
-          className="btn btn-outline-secondary my-2 mr-sm-2"
-          onClick={handleClear}
-        >
-          Clear
-        </button>
-      </form>
-
       <table className="table table-sm table-dark table-bordered table-hover">
         <thead>
           <tr>
-            <th scope="col" onClick={(event) => handleSort(event, "photo")}>Photo</th>
-            <th scope="col" onClick={(event) => handleSort(event, "name")}>Name</th>
-            <th scope="col" onClick={(event) => handleSort(event, "email")}>Email</th>
-            <th scope="col" onClick={(event) => handleSort(event, "phone")}>Phone</th>
-            <th scope="col" onClick={(event) => handleSort(event, "location")}>Location</th>
-            <th scope="col" onClick={(event) => handleSort(event, "gender")}>Gender</th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => requestSort("photo")}
+                className={getClassNamesFor("photo")}
+              >
+                Photo
+              </button>
+            </th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => requestSort("name")}
+                className={getClassNamesFor("name")}
+              >
+                Name
+              </button>
+            </th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => requestSort("email")}
+                className={getClassNamesFor("email")}
+              >
+                Email
+              </button>
+            </th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => requestSort("phone")}
+                className={getClassNamesFor("phone")}
+              >
+                Phone
+              </button>
+            </th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => requestSort("location")}
+                className={getClassNamesFor("location")}
+              >
+                Location
+              </button>
+            </th>
+            <th scope="col">
+              <button
+                type="button"
+                onClick={() => requestSort("gender")}
+                className={getClassNamesFor("gender")}
+              >
+                Gender
+              </button>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {/* Map data for each employee */}
-          {users.map(
+          {items.map(
             ({ picture, name, email, phone, location, gender }, index) => (
               <tr key={index}>
-                <th scope="row"><img src={picture.thumbnail} alt={name.first}></img></th>
-            <td className="col">{name.first}</td>
+                <th scope="row">
+                  <img src={picture.thumbnail} alt={name.first}></img>
+                </th>
+                <td className="col">{name.first}</td>
                 <td className="col">{email}</td>
                 <td className="col">{phone}</td>
                 <td className="col">{location.country}</td>
---
+++ src/components/Table/Table_functionalComponent.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from "react";
+
+/**
+ * * Componet set as a React Hooks component. Component demonstrates the
+ * * use of hooks. Fetched data is retrieved as props from Directory
+ * * container.
+ * @param {*} data Props destructured. Arguements from Directory container
+ */
+const Table = ({ data }) => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    setUsers(data);
+  }, [data]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleClear = () => {
+    setUsers(data);
+    setSearchTerm("");
+  };
+
+  // TODO: sort in descending order
+  // TODO: sort w/ last names
+  const handleSort = (event, field) => {
+    event.preventDefault();
+
+    const userList = [...users];
+    const sortedByField = userList.sort((a, b) => {
+      switch (field) {
+        case "photo":
+          a = a.picture.thumbnail;
+          b = b.picture.thumbnail;
+          break;
+        case "name":
+          a = a.name.first;
+          b = b.name.first;
+          // a = `${a.name.first.toLowerCase()} ${a.name.last.toLowerCase()}`;
+          // b = `${b.name.first.toLowerCase()} ${b.name.last.toLowerCase()}`;
+          break;
+        case "email":
+          a = a.email;
+          b = b.email;
+          break;
+        case "phone":
+          a = a.phone;
+          b = b.phone;
+          break;
+        case "location":
+          a = a.location.country;
+          b = b.location.country;
+          break;
+        case "gender":
+          a = a.gender;
+          b = b.gender;
+          break;
+        default:
+          break;
+      }
+      if (a < b) return -1;
+      else if (a > b) return 1;
+      return 0;
+    });
+    setUsers(sortedByField);
+  };
+
+  // TODO: filter on multiple columns
+  // TODO: filter w/ last names
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const userList = [...users];
+    const filteredUsers = userList.filter((user) => {
+      const regex = new RegExp(searchTerm, "gi");
+      return user.name.first.match(regex);
+    });
+    setUsers(filteredUsers);
+  };
+
+  return (
+    <>
+      <form
+        className="form-inline my-2 my-lg-0"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          className="form-control mr-sm-2"
+          placeholder="Search name"
+          aria-label="Search"
+          name="searchTerm"
+          value={searchTerm}
+          onChange={handleChange}
+          autoComplete="off"
+        />
+        <button
+          className="btn btn-outline-primary my-2 mr-sm-2"
+          type="submit"
+        >
+          Search
+        </button>
+        <button
+          className="btn btn-outline-secondary my-2 mr-sm-2"
+          onClick={handleClear}
+        >
+          Clear
+        </button>
+      </form>
+
+      <table className="table table-sm table-dark table-bordered table-hover">
+        <thead>
+          <tr>
+            <th scope="col" onClick={(event) => handleSort(event, "photo")}>Photo</th>
+            <th scope="col" onClick={(event) => handleSort(event, "name")}>Name</th>
+            <th scope="col" onClick={(event) => handleSort(event, "email")}>Email</th>
+            <th scope="col" onClick={(event) => handleSort(event, "phone")}>Phone</th>
+            <th scope="col" onClick={(event) => handleSort(event, "location")}>Location</th>
+            <th scope="col" onClick={(event) => handleSort(event, "gender")}>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(
+            ({ picture, name, email, phone, location, gender }, index) => (
+              <tr key={index}>
+                <th scope="row"><img src={picture.thumbnail} alt={name.first}></img></th>
+            <td className="col">{name.first}</td>
+                <td className="col">{email}</td>
+                <td className="col">{phone}</td>
+                <td className="col">{location.country}</td>
+                <td className="col">{gender}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Table;
